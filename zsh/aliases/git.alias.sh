0aabb66f4285fdcadcba41894c51caf147a8f3ff
Cole Voss <voss.cole@gmail.com>
2022-08-23 14:11:03 -0500

    zsh updates and git aliases

---
+++ zsh/aliases/git.alias.sh
@@ -0,0 +1,12 @@
+alias gb='git branch'
+
+alias gaa='git add --all'
+
+alias gc='git commit -v'
+alias gcam='git commit -a -m'
+
+alias gcb='git checkout -b'
+alias gco='git checkout'
+
+alias gl='git pull'
+alias gp='git push'
