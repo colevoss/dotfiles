fef589a32e5763c58088affdef99b81778ef2992
Cole Voss <voss.cole@gmail.com>
2023-11-29 10:24:46 -0600

    python install

---
+++ installs/pipenv.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+installPipenv() {
+  echo -e "\n============================="
+  echo -e "Installing Pipenv"
+  echo -e "=============================\n"
+
+  pip install --user pipenv
+}
+
+installPipenv
---
+++ installs/python.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+installPython() {
+  echo -e "\n============================="
+  echo -e "Installing Python"
+  echo -e "=============================\n"
+
+  brew install python
+}
+
+installPython
