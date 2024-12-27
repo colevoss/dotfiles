35a1403eb9ee2f93c96aa3a924ff3b3de5682eac
Cole Voss <voss.cole@gmail.com>
2024-12-27 16:20:14 -0600

    idk

---
+++ zsh/disabled/opam.plugin.zsh
@@ -0,0 +1,3 @@
+# opam configuration
+echo "DOING OPAM"
+[[ ! -r '/Users/colevoss/.opam/opam-init/init.zsh' ]] || source '/Users/colevoss/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
--- zsh/plugins/opam.plugin.zsh
+++
@@ -1,2 +0,0 @@
-# opam configuration
-[[ ! -r /Users/colevoss/.opam/opam-init/init.zsh ]] || source /Users/colevoss/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
